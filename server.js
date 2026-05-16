4be9209ae025f2cdd36ade58d6602a1ff2a601d7
Paola Ruiz Pichardo <paola@Paolas-MacBook-Air.local>
2026-05-16 10:49:01 -0600

    fix: corregir sintaxis server.js

--- server.js
+++ server.js
@@ -79,7 +79,7 @@ async function getGeminiResponse(userId, userMessage) {
 
   const response = await fetch(
     `
-    {https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash:generateContent?key=${GEMINI_API_KEY}`,
+    `https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash:generateContent?key=${GEMINI_API_KEY}`,
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify({
