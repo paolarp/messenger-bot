dabd09e3a2d2e0f92f8729a41fe7b1b8e133200d
Paola Ruiz Pichardo <paola@Paolas-MacBook-Air.local>
2026-05-18 18:54:04 -0600

    add root route

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ require("dotenv").config();
 const express = require("express");
 const app = express();
 app.use(express.json());
+app.get("/", (req, res) => res.status(200).send("OK"));
 
 const VERIFY_TOKEN = process.env.VERIFY_TOKEN;
 const PAGE_ACCESS_TOKEN = process.env.PAGE_ACCESS_TOKEN;
